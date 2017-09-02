4936ad5eff3b375a5fba24bfce732c2796f54e49
shadow-code <15094066966@163.com>
2017-09-02 19:29:42 +0800

    saturday
    完成的18章

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -3,7 +3,24 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from . import models
+
 
 def index(request):
-    '''学习笔记的主页'''
+    """显示主页"""
     return render(request, 'learning_logs/index.html')
+
+
+def topics(request):
+    """显示所有主题"""
+    topics = models.Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+
+def topic(request, topic_id):
+    """显示单个主题及其所有的条目"""
+    topic = models.Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
